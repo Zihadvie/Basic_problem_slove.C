380af5c08f7e0c922c948ced466b9317aa649017
MD. Zihad Islam <159674523+Zihadvie@users.noreply.github.com>
2025-10-04 18:32:48 +0600

    Create Basic_problem_Slove.C
    Input Format
    The input consists of a single integer 
    X
    X, denoting a day in December.
    Output Format
    Output on a new line CHRISTMAS, if it is Christmas and ORDINARY otherwise.

---
+++ Basic_problem_Slove.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+
+ int a,b;
+ a=25;
+ scanf("%d",&b);
+ if(b==a){
+     printf("CHRISTMAS");
+ }else{
+     printf("ORDINARY");
+ }
+ 
+
+return 0;
+
+}
