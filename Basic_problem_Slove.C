fd3e986f10d62c9b83597a6f7357ea111269eab5
MD. Zihad Islam <159674523+Zihadvie@users.noreply.github.com>
2025-10-05 20:15:05 +0600

    Update Basic_problem_Slove.C

--- Basic_problem_Slove.C
+++ Basic_problem_Slove.C
@@ -5,9 +5,9 @@ int main() {
 
  int a,b;
  a=25;
- scanf("%d",&b);
- if(b==a){
-     printf("CHRISTMAS");
+ scanf("%d",&b);   //scan
+ if(b==a){             //if/else
+     printf("CHRISTMAS");      
  }else{
      printf("ORDINARY");
  }
